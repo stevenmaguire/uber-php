462b4524b5c7400d97555c392d6fa36b43ea71b0
Steven Maguire <steven@stevenmaguire.com>
2015-03-23 22:28:26 -0500

    doc blocks on RateLimit

--- src/RateLimit.php
+++ src/RateLimit.php
@@ -4,6 +4,34 @@ class RateLimit
 {
     use GetSetTrait;
 
+    /**
+     * Rate limit upper limit
+     *
+     * @var string
+     */
+    private $limit;
+
+    /**
+     * Rate limit remaining
+     *
+     * @var string
+     */
+    private $remaining;
+
+    /**
+     * Rate limit reset timestamp
+     *
+     * @var string
+     */
+    private $reset;
+
+    /**
+     * Create new RateLimit
+     *
+     * @param string $limit
+     * @param string $remaining
+     * @param string $reset
+     */
     public function __construct($limit, $remaining, $reset)
     {
         $this->limit = $limit;
